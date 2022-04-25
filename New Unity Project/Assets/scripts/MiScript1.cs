53e6624eacf892c2dd6f5fba8da09c6858515d0d
valentinaBolivar <vbolivar88@misena.edu.co>
2022-04-25 13:37:28 -0500

    proyecto base

---
+++ New Unity Project/Assets/scripts/MiScript1.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiScript1 : MonoBehaviour
+{
+    public string mensajeBienvenida;
+    public string miNombre;
+    public int edad2 = 20; 
+    public int edad1 = 17;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+        Debug.Log("Hola_Mundo mi nombre es:" + " miNombre" + " mensajeBienvenida");
+
+        Debug.Log(edad2 + edad1);
+
+
+
+
+
+
+     
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
